b4d6084b85864e8d6e544e7ee276b9eaae90a68c
max-lovell <max_lovell@hotmail.co.uk>
2024-04-29 11:55:10 +0100

    Add name input field to begin screen

--- Assets/begin.cs
+++ Assets/begin.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
+using UnityEngine.SceneManagement; // SceneManager.LoadScene
+using TMPro; // grab text input field
+using System; // String class
+using UnityEditor; // DisplayDialog
 
 public class begin : MonoBehaviour
 {
-    private bool pressed = false; //make sure scene can only be loaded once
-
+    private bool pressed = false; //make sure next scene can only be loaded once
+    public TMP_InputField name_input;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,10 +17,20 @@ public class begin : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey("space") && !pressed){
-            pressed = true;
-            //gameObject.transform.localScale = Vector3.zero; // hide instructions
-            SceneManager.LoadScene("Simple RT");
+        string name = name_input.text;
+        if(Input.GetKeyDown(KeyCode.Return) && !pressed){
+            Debug.Log("Enter");
+            if(name=="Enter your name here" || String.IsNullOrWhiteSpace(name)){ //if not entered valid name
+                EditorUtility.DisplayDialog("Name field empty", "Please enter a name", "Ok");
+            } else {
+                //strip leading/trailing whitespace
+                char[] charsToTrim = {' ', '\''};
+                string result = name.Trim(charsToTrim);
+                //move to first scene
+                pressed = true;
+                //gameObject.transform.localScale = Vector3.zero; // hide instructions
+                SceneManager.LoadScene("Simple RT");
+            }
         }
     }
 }
