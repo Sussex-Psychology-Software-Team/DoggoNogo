94978edc4e4437afeac0e27c8c4236bc143c3740
max-lovell <max_lovell@hotmail.co.uk>
2024-04-29 14:57:51 +0100

    Remove Popup and change to text component show/hide

--- Assets/begin.cs
+++ Assets/begin.cs
@@ -2,32 +2,33 @@ using UnityEngine;
 using UnityEngine.SceneManagement; // SceneManager.LoadScene
 using TMPro; // grab text input field
 using System; // String class
-using UnityEditor; // DisplayDialog
 
 public class begin : MonoBehaviour
 {
-    private bool pressed = false; //make sure next scene can only be loaded once
-    public TMP_InputField name_input;
+    private bool scene_triggered = false; //make sure next scene can only be loaded once
+    public TMP_InputField nameInput;
+    public GameObject warning;
     // Start is called before the first frame update
     void Start()
     {
-        
+        warning.SetActive(false); // false to hide, true to show
     }
 
     // Update is called once per frame
     void Update()
     {
-        string name = name_input.text;
-        if(Input.GetKeyDown(KeyCode.Return) && !pressed){
-            Debug.Log("Enter");
-            if(String.IsNullOrWhiteSpace(name)){ //if name field empty or all whitespace
-                EditorUtility.DisplayDialog("Name field empty", "Please enter a name", "Ok");
+        string name = nameInput.text;
+        if(Input.GetKeyDown(KeyCode.Return) && !scene_triggered){
+            if(String.IsNullOrWhiteSpace(name)){ //if name field empty or all whitespace, and warning not already showing to avoid sticking here
+                warning.SetActive(true); // false to hide, true to show
+                nameInput.ActivateInputField();
+                Debug.Log("Name not entered correctly");
             } else {
                 //strip leading/trailing whitespace
                 char[] charsToTrim = {' ', '\''};
                 string result = name.Trim(charsToTrim);
                 //move to first scene
-                pressed = true;
+                scene_triggered = true;
                 //gameObject.transform.localScale = Vector3.zero; // hide instructions
                 PlayerPrefs.SetString("Name", name);
                 SceneManager.LoadScene("Simple RT");
