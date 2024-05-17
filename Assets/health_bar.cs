5bdc0acf546d88dd134861c61b16ac47e5d0f895
max-lovell <max_lovell@hotmail.co.uk>
2024-05-17 11:59:14 +0100

    rename healthbar and link to bone asset

--- Assets/health_bar.cs
+++ Assets/health_bar.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class health_bar : MonoBehaviour
+public class healthBar : MonoBehaviour
 {
 	public Slider slider;
 	public Gradient gradient;
