10ec17493394730cba462be0069e1895387b6441
Max-Lovell <max_lovell@hotmail.co.uk>
2024-08-07 14:55:07 +0100

    Add jump

--- Assets/Scripts/Dog.cs
+++ Assets/Scripts/Dog.cs
@@ -6,8 +6,13 @@ using UnityEngine.UI; // Image.sprite
 
 public class Dog : MonoBehaviour
 {
+    // Images
     public Sprite[] images; // Array of images of each evolution, increase on level change
     private int i = 0; // image number
+    // Jumping
+    public int maxJumpHeight = 40;
+    public int jumpSpeed = 250;
+
     // Audio feeback
     public AudioSource dogBark; // Barking on early press
     public AudioSource dogChew; // Chewing on fast (i.e. valid) trial
@@ -15,11 +20,15 @@ public class Dog : MonoBehaviour
     public AudioSource dogWhine1; 
     public AudioSource dogWhine2;
 
+
+    // FUNCTIONS ********************************
+    // Images
     public void NextSprite(){
         // Loops through sprites automatically
         gameObject.GetComponent<Image>().sprite = images[++i]; // Note first image just loaded automatically
     }
 
+    // Audio
     public void whine(){
         System.Random rand = new System.Random(); // Random var
         if(rand.Next(2) == 1) dogWhine1.Play(); // .Next() gets new random number, (2) non-inclusive maximum int = 0 or 1
@@ -32,5 +41,38 @@ public class Dog : MonoBehaviour
 
     public void bark(){
         dogBark.Play();
+    }
+
+    // Jumping
+    float startingY; // Initial starting point - ground
+    float yPosition;
+    bool ascending = false;
+    bool descending = false;
+
+    void Start(){
+        startingY = transform.position.y;
+        yPosition = transform.position.y;
+    }
+
+    void Update(){
+
+        // Initiate jump in down arrow
+        if(Input.GetKeyDown(KeyCode.DownArrow)) ascending = true;
+
+        if(ascending){
+            if(yPosition <= startingY+maxJumpHeight) yPosition += jumpSpeed * Time.deltaTime;
+            else{
+                ascending = false;
+                descending = true;
+            }
+        } else if(descending){
+            if(yPosition > startingY) yPosition -= jumpSpeed * Time.deltaTime;
+            else descending = false;
+        }
+        
+        // Change position
+        transform.position = new Vector3(transform.position.x, yPosition, 0);
+        //transform.Translate(new Vector3(0, velocity, 0) * Time.deltaTime);
+        
     }
 }
