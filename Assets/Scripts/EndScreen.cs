837c60f2e0f0dc3bf9eab7f2bfd0020c85612aba
Max-Lovell <max_lovell@hotmail.co.uk>
2024-08-07 13:02:55 +0100

    Comments and abstract to functions in EndScreen

--- Assets/Scripts/EndScreen.cs
+++ Assets/Scripts/EndScreen.cs
@@ -6,42 +6,43 @@ using TMPro; //for TextMeshProUGUI
 
 public class EndScreen : MonoBehaviour
 {
+    // Asset references
     public HealthBar scoreBar;
-
-    public static double mean = 4000.0;
-    public static double sd = 1000.0;
     public TextMeshProUGUI percentileText; // displays score
+    // Average score distribution params
+    public static double mean = 4000.0;
+    public static double sd = 2000.0;
+
     
     // Start is called before the first frame update
     void Start()
     {
         DataManager.Instance.sendData();
-        int score = DataManager.Instance.data.lastTrial().totalScore; //get local copy of player score
-        double percent = Phi(score);
-        string z = percent.ToString("F2");
-        percentileText.text = "You scored better than " + z + "% of participants!";
-        StartCoroutine(scoreAnimator((int)percent));
+        displayRelativeScore();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        //Debug.Log(score);
+    // Score display functions
+    void displayRelativeScore(){
+        // Calculate %
+        int score = DataManager.Instance.data.lastTrial().totalScore; //get copy of player score
+        Debug.Log(score);
+        double zScore = percentileNormCDF(score); // Score under normal as %
+        Debug.Log(zScore);
+        // Change text and Healthbar
+        percentileText.text = "You scored better than " + zScore.ToString("F0") + "% of participants!";
+        StartCoroutine(scoreAnimator((int)zScore));
     }
 
-    IEnumerator scoreAnimator(int percent_score){
-        for (int s=0; s<=percent_score; s++){ //note can make smoother by making SetHealth take a float.
+    IEnumerator scoreAnimator(int percentScore){
+        for (int s=0; s<=percentScore; s++){ //note can make smoother by making SetHealth take a float.
             scoreBar.SetHealth(s);
-            if(s==percent_score && s<100){
-                //Debug.Log("YAY!");
-            }
             yield return new WaitForSeconds(.01f);
         }
     }
 
-    public static double Phi(int percent_score) {
-        //quantile/percentile function in c# - https://stackoverflow.com/questions/1662943/standard-normal-distribution-z-value-function-in-c-sharp
-        double x = Convert.ToDouble(percent_score);
+    public static double percentileNormCDF(int percentScore) { // Phi
+        // Percentile function Polynomial approximation in c# - https://stackoverflow.com/questions/1662943/standard-normal-distribution-z-value-function-in-c-sharp
+        double x = Convert.ToDouble(percentScore); // Convert score to double
         // constants
         double a1 = 0.254829592;
         double a2 = -0.284496736;
@@ -51,19 +52,19 @@ public class EndScreen : MonoBehaviour
         double p = 0.3275911;
             
         // Save the sign of x
-        x = (x - mean) / sd;
+        x = (x - mean) / sd; // Normalise to z score
         int sign = 1;
         if (x < 0)
             sign = -1;
-        x = Math.Abs(x) / Math.Sqrt(2.0);
+        x = Math.Abs(x) / Math.Sqrt(2.0); // Absolute X scaled by sqrt2
             
         // A&S formula 7.1.26
-        double t = 1.0 / (1.0 + p*x);
-        double y = 1.0 - (((((a5*t + a4)*t) + a3)*t + a2)*t + a1)*t * Math.Exp(-x*x);
+        double t = 1.0 / (1.0 + p*x); // Compute t
+        double y = 1.0 - (((((a5*t + a4)*t) + a3)*t + a2)*t + a1)*t * Math.Exp(-x*x); // Polynomial approximation of error function
         
         //output
-        double p_score = 0.5 * (1.0 + sign*y);
-        double percent = p_score*100;
+        double pecentile = 0.5 * (1.0 + sign*y);
+        double percent = pecentile * 100;
         return percent;
     }
 }
