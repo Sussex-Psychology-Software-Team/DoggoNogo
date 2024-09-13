46d43ced9f35d3b76497204c711a3fbd6668d55d
max-lovell <max_lovell@hotmail.co.uk>
2024-09-13 17:03:23 +0100

    Simplified import of getQueryVar on trialManager

--- Assets/Scripts/Simple RT/TrialManager.cs
+++ Assets/Scripts/Simple RT/TrialManager.cs
@@ -96,20 +96,13 @@ public class TrialManager : MonoBehaviour
     }
 
     [DllImport("__Internal")]
-    private static extern IntPtr queryString(string variable);
+    private static extern string queryString(string variable);
 
     public static string GetQueryVariable(string variable) {
         #if UNITY_EDITOR
             return "UNITY EDITOR";
         #elif UNITY_WEBGL && !UNITY_EDITOR
-            IntPtr ptr = queryString(variable);
-            if (ptr != IntPtr.Zero)
-            {
-                string result = Marshal.PtrToStringAnsi(ptr);
-                Marshal.FreeHGlobal(ptr);
-                return result;
-            }
-            return "NONE FOUND";
+            string ptr = queryString(variable);
         #else
             return "NOT EDITOR OR WEBGL";
         #endif
