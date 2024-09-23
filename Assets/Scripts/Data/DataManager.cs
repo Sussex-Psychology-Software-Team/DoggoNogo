560a83e4d040c61f2568a8daf6b1868bcc11b51c
max-lovell <max_lovell@hotmail.co.uk>
2024-09-23 13:05:44 +0100

    Abstraction on sending data to server

--- Assets/Scripts/Data/DataManager.cs
+++ Assets/Scripts/Data/DataManager.cs
@@ -23,17 +23,16 @@ public class DataManager : MonoBehaviour {
         data.metadata.end = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
         Debug.Log(JsonUtility.ToJson(data));
         // Get id for current experimenters and send
-        if(data.metadata.experimentID != "QUERY VAR NOT FOUND"){
-            DataPipeBody currentStudyData = new DataPipeBody(data, data.metadata.experimentID);
-            string currentStudyJSON = currentStudyData.DataPipeJSON();
-            StartCoroutine(dataPipe(currentStudyJSON));
-        }
+        if(data.metadata.experimentID != "QUERY VAR NOT FOUND") SendDataToServer(data.metadata.experimentID);
         // Send to our own location
-        if(data.metadata.experimentID != "VSyXogVR8oTS"){
-            DataPipeBody reblData = new DataPipeBody(data, "VSyXogVR8oTS");
-            string REBLJSON = reblData.DataPipeJSON();
-            StartCoroutine(dataPipe(REBLJSON));
-        }
+        if(data.metadata.experimentID != "VSyXogVR8oTS") SendDataToServer("VSyXogVR8oTS");
+    }
+
+    // Abstract the data sending logic
+    void SendDataToServer(string experimentID) {
+        DataPipeBody studyData = new DataPipeBody(data, experimentID);
+        string studyJSON = studyData.DataPipeJSON();
+        StartCoroutine(dataPipe(studyJSON));
     }
 
     IEnumerator dataPipe(string json) {
