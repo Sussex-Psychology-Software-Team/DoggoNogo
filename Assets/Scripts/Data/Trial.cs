a56747b3d84fa6e8a39e1747859ff8a81d433c40
max-lovell <max_lovell@hotmail.co.uk>
2024-10-04 17:00:39 +0100

    Rename trial number and response type vars in data

--- Assets/Scripts/Data/Trial.cs
+++ Assets/Scripts/Data/Trial.cs
@@ -10,20 +10,20 @@ public class Trial {
     public int totalScore;
     public string trialType;
 
-    public Trial(int trialNumber, double isiVar) {
-        trial_n = trialNumber;
+    public Trial(int trialN, double isiVar) {
+        trialNumber = trialN;
         isi = isiVar;
         rt = -1.0;
         datetime = "";
         trialScore = 0;
         totalScore = -1;
-        trialType = "missed";
+        responseType = "missed";
     }
 
     public void SaveTrial(double rt, string type, int score, int total, double threshold) {
         this.rt = RoundTime(rt, 7);
         this.datetime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
-        this.trialType = type;
+        this.responseType = type;
         this.trialScore = score;
         this.totalScore = total;
         this.threshold = threshold;
