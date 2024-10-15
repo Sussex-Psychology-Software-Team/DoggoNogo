01b4eb5282becaef2747be28f6869b61c9b1093c
Max-Lovell <max_lovell@hotmail.co.uk>
2024-10-15 16:32:59 +0100

    Handle autoplay trial using bool from score manager

--- Assets/Scripts/Simple RT/TrialManager.cs
+++ Assets/Scripts/Simple RT/TrialManager.cs
@@ -11,9 +11,8 @@ using System.Collections.Generic; //for List
 public class TrialManager : MonoBehaviour
 {
     public float[] ISIRange = { 1f, 4f }; // Input floats to Random.Range to return a float
-    
-    // ISI global vars
     double trialISI; // Stores each trial's trialISI for speed of access
+    bool pauseTrial = true;
     // Timer - https://stackoverflow.com/questions/394020/how-accurate-is-system-diagnostics-stopwatch
     Stopwatch timer = new Stopwatch(); // High precision timer: https://learn.microsoft.com/en-us/dotnet/api/system.diagnostics.stopwatch?view=net-8.0&redirectedfrom=MSDN#remarks
 
@@ -44,8 +43,8 @@ public class TrialManager : MonoBehaviour
     void EndTrial(){ // Don't forget missed trials here
         timer.Stop(); // Immediately stop timer
         double rt = timer.Elapsed.TotalSeconds - trialISI; // subtract ISI from time elapsed during press
-        scoreManager.ProcessTrialResult(rt);
-        StartCoroutine(DelayBeforeNextTrial());
+        pauseTrial = scoreManager.ProcessTrialResult(rt);
+        if(!pauseTrial) StartCoroutine(DelayBeforeNextTrial());
     }
 
     void RestartTimer(){
@@ -66,14 +65,14 @@ public class TrialManager : MonoBehaviour
 
     // Update is called once per frame - maybe use FixedUpdate for inputs?
     void Update(){
-        if(trialISI == 0 && Input.GetKeyDown(KeyCode.DownArrow)){ // Note consider trial count or a bool for first trial check
+        if(pauseTrial && Input.GetKeyDown(KeyCode.DownArrow)){ // Note consider trial count or a bool for first trial check
             NewTrial();
         } else if(timer.IsRunning){
             // If ISI ended show bone
             if(timer.Elapsed.TotalSeconds > trialISI && bone.Hidden()){
                 bone.Show(); // End ISI
             }
-            // On down arrow (early or valid press)
+            // On down arrow (early or valid press) or if timer runs out
             if(Input.GetKeyDown(KeyCode.DownArrow) || timer.Elapsed.TotalSeconds > (trialISI + scoreManager.maxRT)){
                 EndTrial();
             }
