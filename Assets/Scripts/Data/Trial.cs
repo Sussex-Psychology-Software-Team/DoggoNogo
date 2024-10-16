7d7bfa8a437adc2c69b498bc522999ce26061eda
Max-Lovell <max_lovell@hotmail.co.uk>
2024-10-16 14:52:14 +0100

    Add stim spec to trial data class

--- Assets/Scripts/Data/Trial.cs
+++ Assets/Scripts/Data/Trial.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic; // Dictionaries
 
 [System.Serializable]
 public class Trial {
@@ -12,6 +13,13 @@ public class Trial {
     public double threshold;
     public bool validTrial;
     public int validTrialCount;
+    // Stim spec
+    public float[] screenSize;
+    public float[] canvasSize;
+    public float canvasScaleFactor;
+    public float[] bonePosition;
+    public float boneOrientation;
+    public float boneScale;
 
     public Trial(int trialN, double isi) {
         trialNumber = trialN;
@@ -27,6 +35,15 @@ public class Trial {
         this.threshold = threshold;
         this.validTrial = validTrial;
         this.validTrialCount = validTrialCount;
+    }
+
+    public void SaveStimulus(Dictionary<string, float> stimSpec){
+        this.screenSize = new float[] {stimSpec["screenWidth"], stimSpec["screenHeight"]};
+        this.canvasSize = new float[] {stimSpec["canvasWidth"], stimSpec["canvasHeight"]};
+        this.canvasScaleFactor = stimSpec["canvasScale"];
+        this.bonePosition = new float[] {stimSpec["x"], stimSpec["y"]};
+        this.boneOrientation = stimSpec["rotation"];
+        this.boneScale = stimSpec["scale"];
     }
 
     double RoundTime(double time, int dp) {
