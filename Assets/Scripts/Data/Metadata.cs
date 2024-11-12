3904b4c51a9b0ce953940480ce52da4ea8df8c8b
Max-Lovell <max_lovell@hotmail.co.uk>
2024-11-12 17:18:06 +0000

    Re-serialise and simplify metadata class

--- Assets/Scripts/Data/Metadata.cs
+++ Assets/Scripts/Data/Metadata.cs
@@ -5,17 +5,11 @@ using UnityEngine;
 [System.Serializable]
 public class Metadata {
     // SESSION-SPECIFIC: Should be NonSerialized
-    [System.NonSerialized]
-    public string sessionID;        // New for each session
-    [System.NonSerialized]
-    public string start;           // Current session start time
-    [System.NonSerialized]
+    public string sessionID = Utility.GenerateRandomId(7) + DateTime.Now.ToString("ddMMyyyy");        // New for each session
+    public string start = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");           // Current session start time
     public string end;            // Current session end time
-    [System.NonSerialized]
     public string startL1;        // Current L1 start time
-    [System.NonSerialized]
     public string endL1;         // Current L1 end time
-    [System.NonSerialized]
     public string userAgentString; // Browser/system info for current session
 
     // EXPERIMENT CONFIGURATION: Can remain serialized
@@ -25,11 +19,6 @@ public class Metadata {
     public string l1n;            // Language/condition parameter
 
     // Constructor
-    public Metadata() {
-        sessionID = Utility.GenerateRandomId(7) + DateTime.Now.ToString("ddMMyyyy");
-        start = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
-    }
-
     public void InitializeWebVariables(){
         // Otherwise datamanager starts on Awake() and UNITY_WEBGL is not true
         experimentID = Utility.GetQueryVariable("datapipe");
@@ -38,7 +27,6 @@ public class Metadata {
         l1n = Utility.GetQueryVariable("l1n");
         userAgentString = GetUserAgent();
     }
-
 
     static string GetUserAgent() {
         #if UNITY_EDITOR
