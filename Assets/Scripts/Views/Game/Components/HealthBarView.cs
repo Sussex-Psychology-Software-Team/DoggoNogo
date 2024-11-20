a145eb498e8213f70fd8a179f99b0c358ed18404
Max-Lovell <max_lovell@hotmail.co.uk>
2024-11-20 16:27:19 +0000

    Simplify healthbar view to use slider

--- Assets/Scripts/Views/Game/Components/HealthBarView.cs
+++ Assets/Scripts/Views/Game/Components/HealthBarView.cs
@@ -3,88 +3,24 @@ using UnityEngine.UI;
 
 public class HealthBarView : MonoBehaviour
 {
-    [Header("UI Components")]
+    [SerializeField] private Slider slider;
     [SerializeField] private Image fillImage;
-    [SerializeField] private Image backgroundImage;
     
-    [Header("Settings")]
-    [SerializeField] private float smoothSpeed = 5f;
-    [SerializeField] private float animationDuration = 0.5f;
-
-    private float _currentHealth;
-    private float _targetHealth;
-    private float _minHealth;
-    private float _maxHealth;
-
-    private void Awake()
+    public void SetMaxValue(float maxValue)
     {
-        ResetHealth();
+        slider.maxValue = maxValue;
+        SetHealth(slider.value);
     }
 
-    public void SetNewTarget(int target)
+    // Note consider a smoother animation on health changes
+    public void SetHealth(float value)
     {
-        _maxHealth = target;
-        _minHealth = 0;
-        ResetHealth();
+        slider.value = value;
     }
 
-    public void SetHealth(float health)
-    {
-        _targetHealth = Mathf.Clamp(health, _minHealth, _maxHealth);
-        
-        // Smoothly animate to new health value
-        if (gameObject.activeInHierarchy)
-        {
-            StartCoroutine(AnimateHealthChange());
-        }
-        else
-        {
-            _currentHealth = _targetHealth;
-            UpdateFillAmount();
-        }
-    }
-
-    public void SetColour(Color color)
+    public void SetColor(Color color)
     {
         fillImage.color = color;
     }
-
-    private void ResetHealth()
-    {
-        _currentHealth = 0;
-        _targetHealth = 0;
-        UpdateFillAmount();
-    }
-
-    private void UpdateFillAmount()
-    {
-        float normalizedHealth = Mathf.InverseLerp(_minHealth, _maxHealth, _currentHealth);
-        fillImage.fillAmount = Mathf.Clamp01(normalizedHealth);
-    }
-
-    private System.Collections.IEnumerator AnimateHealthChange()
-    {
-        float elapsed = 0f;
-        float startHealth = _currentHealth;
-
-        while (elapsed < animationDuration)
-        {
-            elapsed += Time.deltaTime;
-            float t = elapsed / animationDuration;
-            
-            // Use smoothstep for more natural animation
-            t = t * t * (3f - 2f * t);
-            
-            _currentHealth = Mathf.Lerp(startHealth, _targetHealth, t);
-            UpdateFillAmount();
-            
-            yield return null;
-        }
-
-        _currentHealth = _targetHealth;
-        UpdateFillAmount();
-    }
-
-    public float GetMinHealth() => _minHealth;
-    public float GetMaxHealth() => _maxHealth;
+    
 }
