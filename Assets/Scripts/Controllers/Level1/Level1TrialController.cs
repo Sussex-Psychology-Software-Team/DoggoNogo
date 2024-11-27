e49b26ee1a1fbe673947165abceb18a9efbed9ab
Max-Lovell <max_lovell@hotmail.co.uk>
2024-11-27 16:39:12 +0000

    Simplify level trial controller

--- Assets/Scripts/Controllers/Level1/Level1TrialController.cs
+++ Assets/Scripts/Controllers/Level1/Level1TrialController.cs
@@ -14,101 +14,59 @@ public class Level1TrialController : MonoBehaviour
 
     private readonly Stopwatch _timer = new();
     private double _currentTrialIsi;
-    private bool _isTrialActive;
     private bool _isStimulusShown;
+    
+    
+    // Show and hide bone
+    private void ShowStimulus()
+    {
+        var stimulusSpecifications = boneView.Show();
+        _isStimulusShown = true;
+        Level1Controller.Instance.SaveStimulusSpecifications(stimulusSpecifications);
+    }
 
-    // FUNCTIONS
+    private void HideStimulus()
+    {
+        boneView.Hide();
+        _isStimulusShown = false;
+    }
     
     // TRIAL SETUP
     public void StartNewTrial()
     {
-        ResetTrial();
-        _currentTrialIsi = GenerateIsi();
-        Level1Events.NewTrialStarted(_currentTrialIsi);
-        Level1Events.TrialStateChanged(TrialState.WaitingForStimulus);
+        HideStimulus();
+        _currentTrialIsi = Random.Range(gameConfig.ISIRange.x, gameConfig.ISIRange.y); // Note x and y are just the default names on Vector2s
         StartTimer();
     }
-    
-    private void ResetTrial()
-    {
-        _isTrialActive = true;
-        _isStimulusShown = false;
-        boneView.Hide();
-    }
 
-    private double GenerateIsi()
-    {
-        // Note x and y are just the default names on Vector2s
-        return Random.Range(gameConfig.ISIRange.x, gameConfig.ISIRange.y);
-    }
-    
     private void StartTimer()
     {
         _timer.Reset();
         _timer.Start();
     }
-
-    // DURING TRIAL
-    private void HandleStimulusPresentation()
-    {
-        if (_timer.IsRunning && !_isStimulusShown && 
-                _timer.Elapsed.TotalSeconds > _currentTrialIsi)
-        {
-            ShowStimulus();
-        }
-    }
     
-    private void ShowStimulus()
-    {
-        var stimulusSpecifications = boneView.Show();
-        _isStimulusShown = true;
-        Level1Events.StimulusShown(stimulusSpecifications);
-        Level1Events.TrialStateChanged(TrialState.WaitingForResponse);
-    }
-    
-    private void HandleInput()
-    {
-        bool shouldEndTrial = Input.GetKeyDown(KeyCode.DownArrow) || 
-                              _timer.Elapsed.TotalSeconds > (_currentTrialIsi + gameConfig.MaxReactionTime);
-
-        if (shouldEndTrial)
-        {
-            EndTrial();
-        }
-    }
-
     // END TRIAL
     private void EndTrial()
     {
-        if (!_isTrialActive) return;
-
-        _isTrialActive = false;
         _timer.Stop();
-        double reactionTime = CalculateReactionTime();
-        // Perhaps remove events?
-        Level1Events.ReactionTimeRecorded(reactionTime);
-        Level1Events.TrialStateChanged(TrialState.Complete);
+        double reactionTime = _timer.Elapsed.TotalSeconds - _currentTrialIsi;
         Level1Controller.Instance.ProcessTrialResult(reactionTime);
-    }
-    
-    private double CalculateReactionTime()
-    {
-        return _timer.Elapsed.TotalSeconds - _currentTrialIsi;
     }
     
     private void OnDisable()
     {
         _timer.Stop();
-        _isTrialActive = false;
     }
     
-    // RUN UPDATE
+    // RUN TRIAL
     private void Update()
     {
-        if (!_isTrialActive) return;
-
-        HandleStimulusPresentation();
-        HandleInput();
+        if (!_timer.IsRunning) return;
+        if (!_isStimulusShown && _timer.Elapsed.TotalSeconds > _currentTrialIsi) {
+            ShowStimulus();
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow) || _timer.Elapsed.TotalSeconds > (_currentTrialIsi + gameConfig.MaxReactionTime)) {
+            EndTrial();
+        }
     }
-    
 }
